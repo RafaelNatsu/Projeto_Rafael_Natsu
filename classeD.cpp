ae8c6a1cf05c01103bb50cdf2298b993e1a31170
RafaelNatsu <rafaelnatsu99@gmail.com>
2022-02-03 16:38:01 -0300

    Criação do metodo md3

--- classeD.cpp
+++ classeD.cpp
@@ -18,6 +18,11 @@ void classeD::MD2()
     std::cout << "Metodo MD2" << std::endl;
 }
 
+void classeD::MD3()
+{
+    std::cout << "Metodo MD3" << std::endl;
+}
+
 int classeD::getD1()
 {
     return D1;
@@ -36,8 +41,4 @@ float classeD::getD2()
 void classeD::setD2(float _d2)
 {
     D2= _d2;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> parent of 081c3b4 (Revert "Adição da classe D")
--- classeD.hpp
+++ classeD.hpp
@@ -14,14 +14,11 @@ public:
     ~classeD();
     void MD1();
     void MD2();
+    void MD3();
     int getD1();
     void setD1(int _d1);
     float getD2();
     void setD2(float _d2);
 };
 
-<<<<<<< HEAD
 #endif
-=======
-#endif
->>>>>>> parent of 081c3b4 (Revert "Adição da classe D")
